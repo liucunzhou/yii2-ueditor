04f31e4cd61c113192004e00a1ac489c45bac600
liucunzhou <liucunzhou@163.com>
2014-12-24 23:51:09 +0800

    commit the source about yii2-ueditor at first@

---
+++ README.md
@@ -0,0 +1,40 @@
+Yii2 Ueditor
+==============
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist wenyuan/yii2-ueditor "*"
+```
+
+or add
+
+```
+"wenyuan/yii2-ueditor": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?php
+use wenyuan\ueditor\Ueditor;
+
+echo Ueditor::widget(['id'=>"newstext"]); 
+
+?>
+```
+
+更多详情或者批评指正：
+qq		=> 1510033691
+email	=> liucunzhou@163.com
---
+++ Ueditor.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * 
+ * @author 	wenyuan 
+ * Email 	liucunzhou@163.com
+ * qq		1510033691
+ *
+ */
+namespace wenyuan\ueditor;
+
+use Yii;
+use yii\widgets\InputWidget;
+use yii\helpers\Html;
+use yii\helpers\Json;
+use yii\web\View;
+use wenyuan\ueditor\UeditorAsset;
+
+class Ueditor extends InputWidget
+{
+    public $events = [];
+    
+    /*
+     * Tag
+     * @var bool
+     */
+    public $renderTag=true;
+    
+    /*
+     * Initializes the widget
+     */
+    public function init() {
+    	
+    	$this->events = $this->options;
+    	
+        if(empty($this->name)){
+            $this->name=$this->hasModel() ? Html::getInputName($this->model, $this->attribute): $this->id;
+        }
+    	
+        //register css & js
+        $asset = UeditorAsset::register($this->view);
+        
+        //init options
+        parent::init();
+    }
+    
+    /*
+     * Renders the widget
+     */
+    public function run() {
+    	
+        $this->registerScripts();
+        if($this->renderTag===true){
+            echo $this->renderTag();
+        }
+    }
+  
+    /**
+     * render file input tag
+     * @return string
+     */
+    private function renderTag() {
+    	$options = [
+    		'type'	=> 'text/plain',
+    		'name'	=> $this->id,
+    		'id'	=> $this->id
+		];
+    	
+        return Html::script('', $options);
+    }
+    
+    /**
+     * register script
+     */
+    private function registerScripts() {
+        $jsonOptions = Json::encode($this->events);
+        $script = <<<EOF
+var um = UE.getEditor('{$this->id}', {$jsonOptions});
+EOF;
+        $this->view->registerJs($script, View::POS_READY);
+    }
+}
